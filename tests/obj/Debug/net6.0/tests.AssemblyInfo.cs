75915ed46f72b502ca0e9652863747acec26b093
lythienhoang <lythienhoang@gmail.com>
2025-01-12 21:07:51 +0700

    Update

--- tests/obj/Debug/net6.0/tests.AssemblyInfo.cs
+++ tests/obj/Debug/net6.0/tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7f3077a32a286875a6f9e2cdba21bec5689c6d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e529ca2faf5bb9e33bda055a28587878dcfccb0")]
 [assembly: System.Reflection.AssemblyProductAttribute("tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
