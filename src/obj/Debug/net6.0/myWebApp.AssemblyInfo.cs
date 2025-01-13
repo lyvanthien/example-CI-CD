295c0916b0a82a49a130912a10eed5e9c15ac948
lythienhoang <lythienhoang@gmail.com>
2025-01-13 16:33:22 +0700

    Updated Index.cshtml file.

--- src/obj/Debug/net6.0/myWebApp.AssemblyInfo.cs
+++ src/obj/Debug/net6.0/myWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("myWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e529ca2faf5bb9e33bda055a28587878dcfccb0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e933ab08fe9b9fe73fdd63b0517d1913c92d45e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("myWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("myWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
